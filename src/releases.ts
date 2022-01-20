727695cbae7bb7cbdf3556b1f5c4f987859cb5b6
palashmarantas <palashmaran@lambdatest.com>
2022-01-20 20:31:12 +0530

    second commit

--- src/releases.ts
+++ src/releases.ts
@@ -8,7 +8,7 @@ export interface GitAsset {
   url: string
   browser_download_url: string
 }
-// firfit commit sdfsd
+// firfit commit sdfsd sdfsdf
 export interface GitRelease {
   tag_name: string
   assets_url: string
