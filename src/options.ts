a8176c8c43dc3e79c90bbd2035b249951666bd50
palashmarantas <palashmaran@lambdatest.com>
2022-01-31 18:38:29 +0530

    first commit

--- src/options.ts
+++ src/options.ts
@@ -14,7 +14,7 @@ export const version = '{{replace:0}}'
 export interface NexePatch {
   (compiler: NexeCompiler, next: () => Promise<void>): Promise<void>
 }
-// sfsdf sdfsdf "fist ssdfdsdfsdf
+// sfsdf sdfsdf "fist ssdfdsdfsdf sdfsdf
 export interface NexeOptions {
   build: boolean
   input: string
