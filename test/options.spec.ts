147c9439e47e64deecfdf38b7684ba8b91e3e512
palashmarantas <91466368+palashmarantas@users.noreply.github.com>
2022-02-22 21:07:01 +0530

    Update options.spec.ts

--- test/options.spec.ts
+++ test/options.spec.ts
@@ -3,7 +3,7 @@ import { expect } from 'chai'
 import * as path from 'path'
 
 const ext = process.platform === 'win32' ? '.exe' : ''
-/**/
+/* sdf*/
 describe('options', () => {
   describe('cwd', () => {
     it('should use  process.cwd() if nothing is  provided sdfs here', () => {
