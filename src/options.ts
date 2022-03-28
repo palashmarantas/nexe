9c414c286259b9a96864e7f031a4cc1bf323c126
palashmarantas <91466368+palashmarantas@users.noreply.github.com>
2022-03-28 16:57:18 +0530

    Update options.ts

--- src/options.ts
+++ src/options.ts
@@ -14,7 +14,7 @@ export const version = '{{replace:0}}'
 export interface NexePatch {
   (compiler: NexeCompiler, next: () => Promise<void>): Promise<void>
 }
-/* sdfsdf sdf sds */
+/* sdfsdf  sds */
 export interface NexeOptions {
   build: boolean
   input: string
